c4ffc1d0c4c94eecde7fa66b432dbbd78d15fec1
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2025-03-18 22:10:04 +0530

    part 2 of quiz game

--- QuizGame/main.go
+++ QuizGame/main.go
@@ -6,49 +6,67 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
-	fmt.Println(os.Args)
-
+	// flags parsing
 	filename := flag.String("f", "problems.csv", "Problem filename flag eg: -f problems.csv")
-
+	timerValue := flag.Int("t", 30, "Time given to answer a question eg: -t 30")
 	flag.Parse()
 
-	fmt.Println(*filename)
-
+	// get questions from file
 	Questions, err := getQuestion(*filename)
-
 	if err != nil {
 		fmt.Println("err in getQuestion", err)
 		return
 	}
 
+	// Mark, answer temp variable
 	totalMark := len(Questions)
 	mark := 0
 	var answer int
 
+	// channel for timer, input
+	outTimerChan := make(chan bool)
+	inTimerChan := make(chan bool)
+	answerChan := make(chan int)
+
+	go timer(outTimerChan, inTimerChan, *timerValue) // timer goroutine
+
 	for i := 0; i < totalMark; i++ {
-		fmt.Printf("Q%d: %s\n", i+1, Questions[i][0])
+		fmt.Printf("Q%d: %s\n", i+1, Questions[i][0]) // Question printing
 		fmt.Printf("Your Answer: ")
 
-		fmt.Scan(&answer)
+		go Input(answerChan) // get user answer goroutine
+		inTimerChan <- true  // start timer by pass true in inTimeChan channel
 
-		tmp, err := strconv.Atoi(Questions[i][1])
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+		// answer verification and time out checking
+		select {
+		case <-outTimerChan:
+			fmt.Println("\nTimes up!!")
+			i = totalMark + 1
 
-		if answer == tmp {
-			mark++
+		case answer = <-answerChan:
+			inTimerChan <- false
+			tmp, err := strconv.Atoi(Questions[i][1])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			// add 1 mark if answer is correct
+			if answer == tmp {
+				mark++
+			}
 		}
 	}
 
+	// display final Score
 	fmt.Printf("Your score is %d / %d\n", mark, totalMark)
-
 }
 
+// function to get Question List from the Provided filepath
 func getQuestion(csvfilepath string) ([][]string, error) {
 	file, err := os.Open(csvfilepath)
 	if err != nil {
@@ -60,4 +78,33 @@ func getQuestion(csvfilepath string) ([][]string, error) {
 	rec, _ := r.ReadAll()
 
 	return rec, nil
+}
+
+// timer gorooutine
+func timer(outTimerChan, inTimerChan chan bool, t int) {
+	ticker := time.NewTicker(time.Duration(t) * time.Second)
+	ticker.Stop()
+
+	var b bool
+
+	for {
+		select {
+		case <-ticker.C:
+			outTimerChan <- true
+		case b = <-inTimerChan:
+			if b {
+				ticker.Reset(time.Duration(t) * time.Second)
+			} else {
+				ticker.Stop()
+			}
+		}
+	}
+}
+
+// user input goroutine
+func Input(answerChan chan int) {
+	var answer int
+	fmt.Scan(&answer)
+
+	answerChan <- answer
 }
